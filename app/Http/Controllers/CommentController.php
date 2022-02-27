8ed308879192d52f9be4c13c8c24fd9f8c8a4354
Milica <pavlovicm995@yahoo.com>
2022-02-27 22:40:25 +0100

    Send email when a user posts a comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\CommentReceived;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
 use App\Models\Team;
@@ -20,6 +22,7 @@ class CommentController extends Controller
         $comment = new Comment($data);
         $comment->team()->associate($team);
         $comment->user()->associate(auth()->user());
+        Mail::to($team)->send(new CommentReceived($comment));
         $comment->save();
         return back();
     }
