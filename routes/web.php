4fba3efe41e7221994e52df30e6808a558d8d463
Milica <pavlovicm995@yahoo.com>
2022-02-28 11:36:29 +0100

    added news

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,4 +34,5 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/teams/{team}/comments', [CommentController::class, 'store'])->name('createComment');
 
-
+Route::get('/news', [NewsController::class, 'index']);
+Route::get('/news/{news}', [NewsController::class, 'show'])->name('news');
